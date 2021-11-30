598e6c7c99f1e81271924cae11d3b0b66eb38a1a
Anatolii-med <anatolii@github.com>
2021-11-30 23:54:39 +0200

    2 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+stopButton.setAttribute('disabled', 'disabled');
+
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStoptClick);
+
+function onStartClick() {
+	startButton.setAttribute('disabled', 'disabled');
+	stopButton.removeAttribute('disabled');
+	changeBodyColor();
+}
+function onStoptClick() {
+	startButton.removeAttribute('disabled');
+	stopButton.setAttribute('disabled', 'disabled');
+	clearInterval(interval);
+}
+
+function changeBodyColor() {
+	interval = setInterval(() => {
+		const color = getRandomHexColor();
+		body.style.backgroundColor = color;
+	}, 1000);
+}
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
